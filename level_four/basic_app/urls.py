258f8547c92afc7e9f64584ce3df2cae1c3febe5
SadbhavanaBhardwaj <sadbhavanahk@gmail.com>
2020-08-27 02:39:51 +0530

    testing1

--- level_four/basic_app/urls.py
+++ level_four/basic_app/urls.py
@@ -6,7 +6,6 @@ app_name = 'basic_app'
 
 urlpatterns = [
 
-    #path('', views.index, name="index"),
     path('other/', views.other, name="other"),
     path('relative/', views.relative, name="relative"),
 
